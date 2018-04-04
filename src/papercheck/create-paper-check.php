3959789c6f5007c60f028bf2eb3555f1e1aee245
aserediuk_epam <aserediuk_epam@epam.com>
2018-04-04 16:02:47 +0300

    create-paper-check

---
+++ src/papercheck/create-paper-check.php
@@ -0,0 +1,33 @@
+<?php
+require_once (dirname(__FILE__) . '/../../vendor/autoload.php');
+
+$username = $argv[1];
+$password = $argv[2];
+$programToken = urldecode($argv[3]);
+$userToken = urldecode($argv[4]);
+
+$hyperwallet = new \Hyperwallet\Hyperwallet($username, $password);
+
+$paperCheck = new \Hyperwallet\Model\PaperCheck();
+$paperCheck
+    ->setType(\Hyperwallet\Model\PaperCheck::TYPE_PAPER_CHECK)
+	->setBankAccountRelationship(\Hyperwallet\Model\PaperCheck::BANK_ACCOUNT_RELATIONSHIP_SELF)
+	->setProfileType(\Hyperwallet\Model\PaperCheck::PROFILE_TYPE_INDIVIDUAL)
+	->setTransferMethodCountry('US')
+	->setTransferMethodCurrency('USD')
+	->setCity('NY')
+	->setCountry('US')
+	->setShippingMethod(\Hyperwallet\Model\PaperCheck::SHIPPING_METHOD_STANDARD)
+	->setStateProvince('IO')
+	->setPostalCode('12345')
+    ->setAddressLine1('88th str');
+
+try {
+    $paperCheck = $hyperwallet->createPaperCheck($userToken, $paperCheck);
+    echo Utils\Utils::toJson($paperCheck);
+    echo "\n";
+} catch (\Hyperwallet\Exception\HyperwalletException $e) {
+    echo "ERROR:\n";
+    echo $e->getMessage();
+    die("\n");
+}
